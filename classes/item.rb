2598018842a0247ce75bbf7058b1ed67ecef81f3
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-07 19:47:40 +0300

    Create Association

--- classes/item.rb
+++ classes/item.rb
@@ -1,10 +1,12 @@
 class Item
   attr_reader :id
+  attr_accessor :label
 
   def intialize(_id, publish_date, archived: false)
     @id = Random.rand(1...1000)
     @publish_date = publish_date || Time.at(rand * Time.now.to_i).strftime('%Y/%m/%d')
     @archived = archived
+    @label = nil
   end
 
   def can_be_archived?
