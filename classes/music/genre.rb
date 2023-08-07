b94ecbeb03e0c6fc51f375b4ff2a1c226692e854
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-07 17:13:37 +0300

    Create Genre Class

---
+++ classes/music/genre.rb
@@ -0,0 +1,13 @@
+class Genre
+  attr_accessor :items
+  attr_reader :id
+  
+  def intialize (name)
+    @id = Random.rand(1...1000)
+    @name = name
+    @items = []
+  end
+  def add_item(item)
+    @items << item
+  end
+end
