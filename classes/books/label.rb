4685830a2bf683efade7f484f59684cadd4e4819
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-07 17:12:32 +0300

    Create Label Class

---
+++ classes/books/label.rb
@@ -0,0 +1,14 @@
+class Label
+    attr_reader :id
+    attr_accessor :items
+  def initialize(title, color)
+    @id = Random.rand(i..1000)
+    @title = title
+    @color = color
+    @items = []    
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
