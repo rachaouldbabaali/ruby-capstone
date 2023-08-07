3380311ff95c80b532fc2eccba66e8c04f696158
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-07 17:14:47 +0300

    Create Item Class

--- classes/item.rb
+++ classes/item.rb
@@ -1,5 +1,11 @@
 class Item
-
+  attr_reader :id
+  
+  def intialize(id, publish_date, archived: false)
+    @id = Random.rand(1...1000)
+    @publish_date = publish_date || Time.at(rand * Time.now.to_i).strftime('%Y/%m/%d')
+    @archived = archived
+  end
 
   def can_be_archived?
     true
