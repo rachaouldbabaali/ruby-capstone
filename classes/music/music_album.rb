ca909b5f6211eab58ffe33ccec1e969c0873359e
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-08 10:28:21 +0100

    Add accessor

--- classes/music/music_album.rb
+++ classes/music/music_album.rb
@@ -1,8 +1,14 @@
 require '../item'
+require_relative 'genre'
 
 class MusicAlbum < Item
-  def initialize(on_spotify: false)
-    super()
+  attr_accessor :name, :on_spotify, :publish_date, :genre
+
+  def initialize(name, publish_date, id, archived, genre, on_spotify = false)
+    super(id, publish_date, archived)
+    @name = name
     @on_spotify = on_spotify
+    @genre = genre
   end
+
 end
