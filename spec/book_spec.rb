3617fd158120edaa92739c939c6249f7c0bea14d
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-09 21:55:34 +0300

    Tests for Book Class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,39 +1,45 @@
-require_relative '../classes/item'
 require_relative '../classes/books/book'
+require_relative '../classes/item'
 
-RSpec.describe Book do
-  let(:book) { Book.new('good', 'Example Publisher') }
+describe Book do
+  let(:book) { Book.new('Tumaini', 'Maganiko', '2020/08/08') }
 
-  describe '#initialize' do
-    it 'sets cover_state and publisher' do
-      expect(book.cover_state).to eq('good')
-      expect(book.publisher).to eq('Example Publisher')
+  describe '#cover_state' do
+    it 'returns the correct cover state' do
+      expect(book.cover_state).to eq('Tumaini')
+    end
+  end
+
+  describe '#publisher' do
+    it 'returns the correct publisher' do
+      expect(book.publisher).to eq('Maganiko')
+    end
+  end
+
+  describe '#publish_date' do
+    it 'returns the correct publish date' do
+      expect(book.publish_date).to eq('2020/08/08')
+    end
+  end
+
+  describe '#to_hash' do
+    it 'returns a hash with cover_state, publisher, and publish_date' do
+      expected_hash = {
+        cover_state: 'good',
+        publisher: 'Example Publisher',
+        publish_date: '2023-08-09'
+      }
+      book = Book.new('good', 'Example Publisher', '2023-08-09')
+      expect(book.to_hash).to eq(expected_hash)
     end
   end
 
   describe '#can_be_archived?' do
     context 'when cover_state is bad' do
-      it 'returns true' do
-        book.cover_state = 'bad'
-        expect(book.can_be_archived?).to be true
-      end
-    end
-
-    context 'when cover_state is not bad' do
       it 'returns false' do
-        book.cover_state = 'good'
-        expect(book.can_be_archived?).to be false
+        book = Book.new('bad', 'Maganiko', '2020/08/08')
+        expect(book.can_be_archived?).to be_truthy
       end
-    end
-  end
-
-  describe '#to_hash' do
-    it 'returns a hash with cover_state and publisher' do
-      expected_hash = {
-        cover_state: 'good',
-        publisher: 'Example Publisher'
-      }
-      expect(book.to_hash).to eq(expected_hash)
     end
   end
 end
