7c93cce1e009a53598f2cc3dc6ec95411bc4249a
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-09 14:21:17 +0300

    Update Application Method

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
+require_relative 'classes/books/addbook'
+require_relative 'classes/list'
+require_relative 'classes/save_data'
+require_relative 'classes/books/addlabel'
+
 class App
+  attr_accessor :books
+
+  def initialize
+    @save_data = SaveData.new
+    @books = @save_data.read_data('books.json')
+    @labels = @save_data.read_data('labels.json')
+  end
+
   def menu
     puts '1 - List all Books'
     puts '2 - List all music albums'
@@ -12,15 +25,36 @@ class App
     puts '10 - Add a music album'
     puts '11 - Add a movie'
     puts '12 - Add a game'
-    puts '13 - Exit applicattion'
+    puts '13 - Add a label'
+    puts '14 - Exit applicattion'
   end
 
-  def run
+  def run(options)
     loop do
       menu
       user_option = gets.chomp.to_i
 
-      break if user_option == 13
+      if options.key?(user_option)
+        options[user_option].call
+      else
+        puts 'Invalid Selection'
+      end
+
+      break if user_option == 14
     end
+  end
+
+  def application
+    add_book = AddBook.new
+    list_all_books = List.new
+    addlabel = AddLabel.new
+
+    options = {
+      9 => -> { add_book.add_book(@books) },
+      1 => -> { list_all_books.list_all_books },
+      13 => -> { addlabel.add_label(@labels) }
+    }
+
+    run(options)
   end
 end
