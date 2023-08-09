52e00e1c14485b193d0e7576db2d74c56db31305
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-09 23:56:17 +0300

    Create Database Schema

---
+++ database/schema.sql
@@ -0,0 +1,31 @@
+-- create database for catalog
+CREATE DATABASE IF NOT EXISTS catalog_of_things;
+
+-- create table my_catalog
+CREATE TABLE my_catalog(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  genre_id     INT NOT NULL,
+  author_id    INT NOT NULL,
+  label_id     INT NOT NULL,
+  publish_date DATE NOT NULL,
+  archived     BOOLEAN NOT NULL,
+  CONSTRAINT label_fk FOREIGN KEY (label_id) REFERENCES Labels (id),
+  CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES Authors (id),
+  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES Genres (id)
+);
+
+-- create table for books
+CREATE TABLE books(
+  ID SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  publisher VARCHAR(30),
+  cover_state VARCHAR(30),
+  label_ID INT REFERENCES labels(ID)
+);
+
+-- create table for labels
+CREATE TABLE labels(
+  ID SERIAL PRIMARY KEY,
+  title VARCHAR(30),
+  color VARCHAR(30)
+);
