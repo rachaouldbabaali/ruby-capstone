38667a24f12955890167e3c35bb140d6e4c643bf
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-09 14:12:45 +0300

    Define Method

--- classes/books/book.rb
+++ classes/books/book.rb
@@ -1,6 +1,8 @@
-require '../item'
+require_relative '../item'
 
 class Book < Item
+  attr_accessor :cover_state, :publisher
+
   def initialize(cover_state, publisher)
     super()
     @cover_state = cover_state
@@ -8,6 +10,13 @@ class Book < Item
   end
 
   def can_be_archived?
-    can_be_archived || cover_state == 'bad'
+    can_be_archived || @cover_state == 'bad'
+  end
+
+  def to_hash
+    {
+      cover_state: @cover_state,
+      publisher: @publisher
+    }
   end
 end
