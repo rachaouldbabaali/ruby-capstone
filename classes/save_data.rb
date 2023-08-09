5a217b6c35fc2a56a8d7f72d0e279721446cd784
tumainimaganiko <tumainimaganiko@gmail.com>
2023-08-09 14:16:20 +0300

    Create Read Data method

--- classes/save_data.rb
+++ classes/save_data.rb
@@ -2,5 +2,14 @@ require 'json'
 require_relative 'books/book'
 
 class SaveData
+  def read_data(file_name)
+    if File.exist?(file_name)
+      file = File.read(file_name)
+      JSON.parse(file, symbolize_names: true)
+    else
+      []
+    end
+  end
+
   
 end
