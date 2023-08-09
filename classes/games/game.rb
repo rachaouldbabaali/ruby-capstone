7eb82fb8f75174d0430726a6283a16c970b596ea
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-09 18:49:06 +0100

    Implement game class

--- classes/games/game.rb
+++ classes/games/game.rb
@@ -1,9 +1,18 @@
 require '../item'
+require 'date'
 
 class Game < Item
-  def initialize(multiplayer, last_played_at)
-    super()
+
+  attr_reader :multiplayer, :last_played_at
+  attr_accessor :author
+
+  def initialize(id, publish_date, archived, multiplayer, last_played_at)
+    super(id, publish_date, archived)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && Date.today - Date.parse(@last_played_at) > 730
   end
 end
