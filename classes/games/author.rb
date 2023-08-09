4d83fb2dd0a3444989f93bdb380c345790b66940
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-09 18:50:40 +0100

    Implement author class

--- classes/games/author.rb
+++ classes/games/author.rb
@@ -1,15 +1,16 @@
 class Author
-  attr_accessor :items
+  attr_accessor :name, :last_name, :items
   attr_reader :id
 
   def initialize(first_name, last_name)
-    @id = Random.rand(1...1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
+    @id = rand(1..1000)
   end
 
-  def add_item(item)
-    @items << item
+  def add_item(game)
+    @items << game
+    game.author = self
   end
 end
