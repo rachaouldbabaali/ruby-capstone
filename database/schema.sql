9fc8d491df00fdad69c41f730c9936e0e94cac1e
Racha OULDBABAALI <rachaould@gmail.com>
2023-08-10 01:39:50 +0100

    Create  table for game and author

--- database/schema.sql
+++ database/schema.sql
@@ -29,3 +29,18 @@ CREATE TABLE genre(
   ID SERIAL PRIMARY KEY,
   name VARCHAR(50)
 );
+
+-- Create authors table
+create table authors(
+  id serial not null primary key,
+  name varchar(100),
+  last_name varchar(100)
+);
+
+-- create games table
+create table games(
+  id serial not null primary key,
+  multiplayer varchar(100),
+  last_played date,
+  publish_date date
+);
